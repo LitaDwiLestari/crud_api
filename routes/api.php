e41c3748b5f7e50d0e0b458f921d11d8354dd078
LitaDwiLestari <litadwil98@gmail.com>
2023-06-20 22:49:02 +0700

    First commit

---
+++ routes/api.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\MakulController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/mahasiswas', [MahasiswaController::class, 'index']);
+    Route::post('/mahasiswas', [MahasiswaController::class, 'store']);
+    Route::get('/mahasiswas/{mahasiswa}', [MahasiswaController::class, 'show']);
+    Route::put('/mahasiswas/{mahasiswa}', [MahasiswaController::class, 'update']);
+    Route::delete('/mahasiswas/{mahasiswa}', [MahasiswaController::class, 'destroy']);
+
+    Route::get('/dosens', [DosenController::class, 'index']);
+    Route::post('/dosens', [DosenController::class, 'store']);
+    Route::get('/dosens/{dosen}', [DosenController::class, 'show']);
+    Route::put('/dosens/{dosen}', [DosenControllerr::class, 'update']);
+    Route::delete('/dosens/{dosen}', [DosenController::class, 'destroy']);
+
+    Route::get('/makuls', [MakulController::class, 'index']);
+    Route::post('/makuls', [MakulController::class, 'store']);
+    Route::get('/makuls/{makul}', [MakulController::class, 'show']);
+    Route::put('/makuls/{makul}', [MakulController::class, 'update']);
+    Route::delete('/makuls/{makul}', [MakulController::class, 'destroy']);
+});
